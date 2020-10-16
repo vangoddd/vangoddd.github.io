4293ddcdf6e3d672bbc5b94fc858f146082968de
BuildTools <unconfigured@null.spigotmc.org>
2020-10-16 20:58:25 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function setDate(){
+
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+    document.getElementById("date").innerHTML = date;
+    
+}
